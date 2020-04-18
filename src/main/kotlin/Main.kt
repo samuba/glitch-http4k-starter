ffc767681b2e489502e32770b1c13ad352b8d997
Samuel Bach <samuel.bach@daimler.com>
2020-04-18 13:29:30 +0200

    add intellij idea files to gitignore

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,8 @@
 package glitch
 
-import org.http4k.client.OkHttp
-import org.http4k.core.Filter
-import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
-import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
-import org.http4k.core.then
-import org.http4k.filter.CachingFilters
 import org.http4k.routing.bind
 import org.http4k.routing.path
 import org.http4k.routing.routes
@@ -23,7 +17,7 @@ fun main() {
         },
         "/greet/{name}" bind GET to { req ->
             val name = req.path("name")
-            Response(OK).body("hello ${name}")
+            Response(OK).body("hello $name")
         }
     )
 
